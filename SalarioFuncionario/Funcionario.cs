ce3e46993cdd4f002f5d449e27b2727c140243fe
Leandro Andrade <leandrade666@gmail.com>
2024-03-23 22:04:56 -0300

    adicionando arquivos

--- SalarioFuncionario/Funcionario.cs
+++ SalarioFuncionario/Funcionario.cs
@@ -10,15 +10,36 @@ namespace SalarioFuncionario
         public double SalarioBruto;
         public double Imposto;
 
+
+        public Funcionario (string nome, double salarioBruto, double imposto)
+        {
+            Nome = nome;
+            SalarioBruto = salarioBruto;
+            Imposto = imposto;
+
+        }
+
         public double CalcularSalarioLIquido()
         {
             double SalarioLiquido = SalarioBruto - (SalarioBruto * Imposto / 100);
             return SalarioLiquido;
 
+            
 
 
 
+
+
+
+    }
+
+
+        public override string ToString()
+        {
+            return $"Nome: {Nome} \n Salario Bruto: {SalarioBruto} \n Imposto {Imposto}";
         }
+
+
         public void AumentarSalario(  double Imposto)
         {
 
--- SalarioFuncionario/Program.cs
+++ SalarioFuncionario/Program.cs
@@ -7,14 +7,18 @@ namespace SalarioFuncionario
         static void Main(string[] args)
         {
 
-            Funcionario Funcionario = new Funcionario();
+            
             Console.WriteLine("Informe os dados do Funcionário:\n");
             Console.WriteLine("Informe o nome do Funcionário:");
-             Funcionario.Nome = Console.ReadLine();
+             string nome = Console.ReadLine();
             Console.WriteLine("Informe o salário bruto  do Funcionário:");
-             Funcionario.SalarioBruto = double.Parse(Console.ReadLine());
+             double  salarioBruto = double.Parse(Console.ReadLine());
             Console.WriteLine("Informe a porcentagem de imposto sobre o salário bruto  do Funcionário:");
-             Funcionario.Imposto = double.Parse(Console.ReadLine());
+             double imposto = double.Parse(Console.ReadLine());
+
+            Funcionario Funcionario = new Funcionario(nome,salarioBruto, imposto);
+            Console.WriteLine(Funcionario);
+
             double SalarioLiquido = Funcionario.CalcularSalarioLIquido(); 
             Console.WriteLine($"O funcionário {Funcionario.Nome} tem um salário Líquido de R$ {SalarioLiquido.ToString("F2")}");
             Console.WriteLine("Informe a porcentagem de aumento que será dado sobre seu salário bruto");
